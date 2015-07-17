90c25d1df13ad8e8106312d4875e8c556abc2cb1
Wang Yichao <wangyichao@r-225-96-25-172.comp.nus.edu.sg>
2015-07-17 15:28:17 +0800

    init explore page

--- MoMo/www/js/controllers.js
+++ MoMo/www/js/controllers.js
@@ -1,28 +1,85 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('ExploreCtrl', function ($scope, $ionicPopup, Missions) {
+    $scope.missions = Missions.all();
+    $scope.remove = function (mission) {
+        Missions.remove(mission);
+    }
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // Triggered on a button click, or some other target
+    $scope.showPopup = function() {
+        $scope.data = {}
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+            template: '<input type="password" ng-model="data.wifi">',
+            title: 'Enter Wi-Fi Password',
+            subTitle: 'Please use normal things',
+            scope: $scope,
+            buttons: [
+                { text: 'Cancel' },
+                {
+                    text: '<b>Save</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if (!$scope.data.wifi) {
+                            //don't allow the user to close unless he enters wifi password
+                            e.preventDefault();
+                        } else {
+                            return $scope.data.wifi;
+                        }
+                    }
+                }
+            ]
+        });
+        myPopup.then(function(res) {
+            console.log('Tapped!', res);
+        });
+        $timeout(function() {
+            myPopup.close(); //close the popup after 3 seconds for some reason
+        }, 3000);
+    };
+
+    $scope.showConfirm = function() {
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Consume Ice Cream',
+            template: 'Are you sure you want to eat this ice cream?'
+        });
+        confirmPopup.then(function(res) {
+            if(res) {
+                console.log('You are sure');
+            } else {
+                console.log('You are not sure');
+            }
+        });
+    };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('MissionDetailCtrl', function ($scope, $stateParams, Missions) {
+    $scope.mission = Missions.get($stateParams.missionId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
+})
+
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
 });
--- MoMo/www/js/services.js
+++ MoMo/www/js/services.js
@@ -1,47 +1,58 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Missions', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+    var missions = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        location: 'Temasek',
+        time: 'one hour',
+        message: 'You on your way?',
+        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        location: 'PGP',
+        message: 'Hey, it\'s me',
+        time: 'one hour',
+        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        location: 'KE',
+        message: 'I should buy a boat',
+        time: 'one hour',
+        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    }, {
+        id: 3,
+        name: 'Perry Governor',
+        location: 'RH',
+        time: 'one hour',
+        message: 'Look at my mukluks!',
+        face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    }, {
+        id: 4,
+        name: 'Mike Harrington',
+        location: 'Shears',
+        time: 'one hour',
+        message: 'This is wicked good ice cream.',
+        face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    }];
+
 
   return {
     all: function() {
-      return chats;
+      return missions;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(mission) {
+      missions.splice(missions.indexOf(mission), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(missionId) {
+      for (var i = 0; i < missions.length; i++) {
+        if (missions[i].id === parseInt(missionId)) {
+          return missions[i];
         }
       }
       return null;
